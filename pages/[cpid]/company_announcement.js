df10506a66ab197c6642c0e9f3104a844f94c1e2
Mukul Kadyan <mukulkadyan202@gmail.com>
2022-03-19 23:35:01 +0530

    Update company_announcement.js

--- pages/[cpid]/company_announcement.js
+++ pages/[cpid]/company_announcement.js
@@ -18,8 +18,8 @@ export async function getServerSideProps(context) {
   let {params, query} = context;
   let cpid = params.cpid;
   let cpname = query.cpname;
-  const response = await fetch(`https://dalpha-server-ism.herokuapp.com/api/v1/announcements?id=${cpid}`);
-  let dataurl = await fetch(`https://dalpha-server-ism.herokuapp.com/api/v1/download.csv?id=${cpid}`);
+  const response = await fetch(`https://dalpha-server-inter-iit.herokuapp.com/api/v1/announcements?id=${cpid}`);
+  let dataurl = await fetch(`https://dalpha-server-inter-iit.herokuapp.com/api/v1/download.csv?id=${cpid}`);
   let data = await response.json()
   return {
     props: {
